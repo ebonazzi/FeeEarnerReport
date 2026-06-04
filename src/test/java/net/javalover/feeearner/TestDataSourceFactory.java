c682c12b1e1c9d8569e1b9f4da2a1d3917b2b84e
Elio Bonazzi <eliobonazzi@gmail.com>
2026-06-04 16:56:24 +1000

    fix: remove dead props loading, guard getResource() null in TestDataSourceFactory

--- src/test/java/net/javalover/feeearner/TestDataSourceFactory.java
+++ src/test/java/net/javalover/feeearner/TestDataSourceFactory.java
@@ -5,33 +5,27 @@ import com.zaxxer.hikari.HikariDataSource;
 import net.javalover.feeearner.config.CredentialLoader;
 
 import javax.sql.DataSource;
-import java.util.Properties;
+import java.net.URL;
 
 public final class TestDataSourceFactory {
 
     private TestDataSourceFactory() {}
 
     public static DataSource create() {
-        var props = new Properties();
-        try (var in = TestDataSourceFactory.class
-                .getResourceAsStream("/test-credentials.properties")) {
-            if (in == null)
-                throw new IllegalStateException(
-                    "test-credentials.properties not found in src/test/resources/");
-            props.load(in);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        var creds = CredentialLoader.load(
-            TestDataSourceFactory.class
-                .getResource("/test-credentials.properties")
-                .getPath());
+        URL resource = TestDataSourceFactory.class
+                .getResource("/test-credentials.properties");
+        if (resource == null)
+            throw new IllegalStateException(
+                "test-credentials.properties not found in src/test/resources/");
+
+        var creds = CredentialLoader.load(resource.getPath());
 
         var config = new HikariConfig();
         config.setJdbcUrl(creds.jdbcUrl());
         config.setUsername(creds.username());
         config.setPassword(creds.password());
         config.setMaximumPoolSize(2);
+        config.setConnectionTimeout(5_000);
         return new HikariDataSource(config);
     }
 }
